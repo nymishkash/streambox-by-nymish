bb3731646615f921af185361b3dad81d1ebdeee7
nymishkash <reachnymish@gmail.com>
2024-02-07 23:37:25 +0530

    uploading files

---
+++ script.js
@@ -0,0 +1,118 @@
+const musicContainer = document.getElementById('audio-container');
+const playBtn = document.getElementById('play');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+
+const audio = document.getElementById('audio');
+const progress = document.getElementById('progress');
+const progressContainer = document.getElementById('progress-container');
+const title = document.getElementById('title');
+const cover = document.getElementById('cover');
+
+// Song titles
+const songs = [
+  'Father Stretch My Hands Pt. 1 - Kanye West',
+  'Fix You - Coldplay',
+  'Hotel California - Eagles',
+  'Saanjh - Utsavi Jha',
+  'Sultans of Swing - Dire Straits',
+  'Sweater Weather - The Neighbourhood',
+  ];
+
+// Keep track of song
+let songIndex = 1;
+
+// Initially load song details into DOM
+loadMusic(songs[songIndex]);
+
+// Update song details
+function loadMusic(song) {
+  title.innerText = song;
+  audio.src = `music/${song}.mp3`;
+  cover.src = `images/${song}.jpg`;
+}
+
+// Play song
+function playSong() {
+  musicContainer.classList.add('play');
+  playBtn.querySelector('i.fas').classList.remove('fa-play');
+  playBtn.querySelector('i.fas').classList.add('fa-pause');
+
+  audio.play();
+}
+
+// Pause song
+function pauseSong() {
+  musicContainer.classList.remove('play');
+  playBtn.querySelector('i.fas').classList.add('fa-play');
+  playBtn.querySelector('i.fas').classList.remove('fa-pause');
+
+  audio.pause();
+}
+
+// Previous song
+function prevSong() {
+  songIndex--;
+
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+
+  loadMusic(songs[songIndex]);
+
+  playSong();
+}
+
+// Next song
+function nextSong() {
+  songIndex++;
+
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
+
+  loadMusic(songs[songIndex]);
+
+  playSong();
+}
+
+// Update progress bar
+function progressBar(e) {
+  const { duration, currentTime } = e.srcElement;
+  const progressPercent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPercent}%`;
+}
+
+// Set progress bar
+function changeTimeStamp(e) {
+  const width = this.clientWidth
+  const clickX = e.offsetX
+  const duration = audio.duration
+
+  audio.currentTime = (clickX / width) * duration;
+}
+
+// Event listeners
+playBtn.addEventListener('click', () => {
+  const isPlaying = musicContainer.classList.contains('play');
+
+  if (isPlaying) {
+    pauseSong();
+  } else {
+    playSong();
+  }
+});
+
+// Change song
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+
+// Time/song update
+audio.addEventListener('timeupdate', progressBar);
+
+// Click on progress bar
+progressContainer.addEventListener('click', changeTimeStamp);
+
+// Song ends
+audio.addEventListener('ended', nextSong);
+
